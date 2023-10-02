38336709956583495a2dfad83fe665743d8f644a
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-10-02 01:26:50 +0100

    Add files via upload

---
+++ src/app/componentes/gestion-tareas/gestion-tareas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-tareas',
+  templateUrl: './gestion-tareas.component.html',
+  styleUrls: ['./gestion-tareas.component.css']
+})
+export class GestionTareasComponent {
+
+}
